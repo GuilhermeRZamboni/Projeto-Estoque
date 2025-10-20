736b2c4200e7423651bbb1bd87949208b17e1250
Guilherme <gui.rzamb@gmail.com>
2025-10-20 09:48:20 -0300

    funcao adicionar produto

--- backend/funcao.py
+++ backend/funcao.py
@@ -18,3 +18,18 @@ def criar_tabela():
             cursor.close()
             conexao.close()
 # criar_tabela()
+
+
+def adicionar_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("""INSERT INTO produtos (nome, categoria, preco, quantidade)
+                              VALUES (%s, %s, %s, %s);""",
+                           (nome, categoria, preco, quantidade))
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao adicionar produto: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
